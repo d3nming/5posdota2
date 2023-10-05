b4130ac00be905b9202e923fd66bad2fe2b900c4
d3nming <115289162+d3nming@users.noreply.github.com>
2023-10-05 21:43:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function updateTimer() {
+    const endDate = new Date("2023-10-12T12:30:00Z"); // Установите дату и время окончания таймера
+    const now = new Date();
+    const timeDifference = endDate - now;
+
+    if (timeDifference <= 0) {
+        // Таймер истек, можно выполнить нужные действия
+        document.getElementById("days").textContent = "00";
+        document.getElementById("hours").textContent = "00";
+        document.getElementById("minutes").textContent = "00";
+        document.getElementById("seconds").textContent = "00";
+        return;
+    }
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = String(days).padStart(2, '0');
+    document.getElementById("hours").textContent = String(hours).padStart(2, '0');
+    document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
+}
+
+setInterval(updateTimer, 1000); // Обновлять таймер каждую секунду
